5b19396ca185fed81c4fc9dda6c28b7af6f703cf
Ga Jun Young <ga.young@personio.de>
2024-05-11 10:30:58 +0100

    Init code

---
+++ src/test/kotlin/com/gajyoung/sonarqubewithmultimodule/SonarqubeWithMultimoduleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gajyoung.sonarqubewithmultimodule
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SonarqubeWithMultimoduleApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
